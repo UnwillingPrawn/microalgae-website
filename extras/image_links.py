2734c86fab1a28349fbda7d080078521f6df7e0e
UnwillingPrawn <remydardare@yahoo.com.au>
2025-05-27 11:19:30 +1000

    Add files via upload

---
+++ extras/image_links.py
@@ -0,0 +1,87 @@
+# extras/image_links.py
+
+image_data = {
+    "cyanobacteria": [
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/cyanobacteria/oscillatoria1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/cyanobacteria/oscillatoria2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/cyanobacteria/oscillatoria3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/cyanobacteria/oscillatoria4.jpg"
+    ],
+    "diatoms": [
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/astrionella1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/astrionella2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/bellerochea1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/bellerochea2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/bellerochea3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/bellerochea4.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/chaetoceros1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/chaetoceros2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/chaetoceros3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/entomoneis1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus4.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus5.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus6.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/leptocylindrus7.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/navicula1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/navicula2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia4.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia5.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia6.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia7.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia8.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/nitzschia9.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/odontella1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/odontella2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/odontella3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/odontella4.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/pleurosigma1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/pleurosigma2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/thalassionema1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/thalassionema2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/thalassionema3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/thalassiosira1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/Thalassiosira2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/diatoms/triceratium.jpg"
+    ],
+    "dinoflagellates": [
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Ceratium1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Ceratium2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Gymnodinium1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Gymnodinium2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Gymnodinium3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Gymnodinium4.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Gymnodinium5.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/prorocentrum1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/prorocentrum2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/prorocentrum3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/prorocentrum4.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/prorocentrum5.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/Protoperidinium1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/scripsiella1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/scripsiella2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/scripsiella3.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/dinoflagellates/scripsiella4.jpg"
+    ],
+    "picoplankton": [
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/picoplankton/Nannochloropsis1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/picoplankton/Nannochloropsis2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/picoplankton/Nannochloropsis3.jpg"
+    ],
+    "raphidophytes": [
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/raphidophytes/Heterosigma1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/raphidophytes/Heterosigma2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/raphidophytes/Heterosigma3.jpg"
+    ],
+    "zooplankton": [
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/zooplankton/copepods1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/zooplankton/copepods2.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/zooplankton/euplotes1.jpg",
+        "https://raw.githubusercontent.com/UnwillingPrawn/microalgae-website/main/static/images/zooplankton/euplotes2.jpg"
+    ]
+}
